bcb549bee99ac44ecfec770c5b3bdf35cf686023
Ameer12348 <ameerarif12348@gmail.com>
2023-12-20 11:34:43 +0500

    adding vercel.json

--- database/databaseconnect.js
+++ database/databaseconnect.js
@@ -4,7 +4,7 @@ const colors = require('colors')
 
 const databaseconnect = async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect("mongodb+srv://ameer:1276@dans.3magesj.mongodb.net/dans?retryWrites=true&w=majority")
         console.log('database connection established'.bgGreen);
     }
     catch(er){
